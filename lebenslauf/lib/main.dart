2af9c8dd7e47cbdea2af9025aeee4b7c081dc3bc
Zumsel <zumselito@gmial.com>
2025-01-11 22:03:20 +0000

    BrightnessModeSwitch

--- lebenslauf/lib/main.dart
+++ lebenslauf/lib/main.dart
@@ -8,24 +8,38 @@ class CVApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+
     return ValueListenableBuilder<ThemeMode>(
       valueListenable: _notifier,
       builder: (_, mode, __) {
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           theme: ThemeData.light(),
           darkTheme: ThemeData.dark(),
           themeMode: mode,
           home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Herzlich Willkommen'),
+              actions: [
+                ElevatedButton(
+                  onPressed: () {
+                    _notifier.value = mode == ThemeMode.light
+                        ? ThemeMode.dark
+                        : ThemeMode.light;
+                    isDarkMode = !isDarkMode;
+                  },
+                  child: Icon(isDarkMode
+                      ? Icons.wb_sunny_outlined
+                      : Icons.bubble_chart),
+                ),
+              ],
+            ),
             body: Center(
-              child: Column(
-                children:[
-                  Text('${_notifier.value}'),
-                  ElevatedButton(
-                    onPressed: () => _notifier.value = mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light,
-                    child: Text('Toggle Theme')
-                  )
-                ]            
-              ),
+              child: Column(children: [
+                Text('${MediaQuery.platformBrightnessOf(context)}'),
+              ]),
             ),
           ),
         );
